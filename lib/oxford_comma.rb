80570bffefdbe187f73c3c4800c9405c96eb92c4
Shawn Koski <shawnnkoski@gmail.com>
2018-04-01 15:18:34 +0000

    Oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    string = ""
+    array.each do |word|
+      array.find_index(word) == array.size - 1 ? string += "and #{word}" : string += "#{word}, "
+    end
+    string
+  end
 end
